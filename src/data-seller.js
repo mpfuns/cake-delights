f89a42b5e4ac500796275aaae8f876c004f4b663
Monet <monetpaul23@gmail.com>
2023-07-23 19:14:24 -0400

    working on  high selling data

---
+++ src/data-seller.js
@@ -0,0 +1,23 @@
+export default[
+{ title:"Ice Cream Sundae Banana Split Cake", 
+img:"https://monet935005.invisionapp.com/assets/A_MGFjZjlkZDY2YjhlM2JmOQC5hldKnf4OH8jNtU6cNing5fKeWaX4RokGGeJB3uWs0VnTjtjPxdLt6yVfdzZasETeZOyEUZTkIDSofp_L_249I6UKYsJkyCQuMJCL8dNh", 
+rating:4.9,
+cost: 99.99,
+salePrice:74.99},
+{ title:"Honey Bee Cake", 
+img:"https://monet935005.invisionapp.com/assets/A_MGFjZjlkZDY2YjhlM2JmOYO3UDD4nmK6E2aIB03HtpMCIzTIPjBuoe2BiqTAhG3Sc7d-_vjcirXIMgfb1RcIt-uCfPUz1MKMGTG43Ha1lrokZYSm41pRK6qYCUxF4t3C", 
+rating:4.7,
+cost:74.99,
+salePrice: 64.99},
+{ title:"Soulmate Cake", 
+img:"https://monet935005.invisionapp.com/assets/A_MGFjZjlkZDY2YjhlM2JmOZnRJBvMhc12TcGgJNdhoblrqP-U_OAGgXcy4CQEXCWcmWAR2mj4_8BjcOgsmgDtm2b57r8bGnuqvzkD1LobDy05JOpHiqTK61g7UZ9qSD4v", 
+rating:4.8,
+cost:34.50,
+salePrice:""},
+{ title:"Ice Cream Sundae Banana Split Cake ", 
+img:"", 
+rating:"",
+cost:"",
+salePrice:""}
+
+]
