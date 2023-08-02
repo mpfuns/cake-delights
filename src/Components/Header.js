c5647fcf9ceafd2ea6a1e68ec9eaae4347e10136
Monet <monetpaul23@gmail.com>
2023-08-02 12:18:10 -0400

    working  highlights  done with html

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ export default function Header (props){
             <div className="header__container">
                 <div className="header__description">
                     <h1>Welcome to <span className="neon-blue">Cake Delights</span>, the ultimate destination for all your cake cravings in the USA.</h1>
-                    <h2>As the best online cake shop, we take pride in offering a delightful assortment of handcrafted cakes that are sure to sweeten every occasion and celebration.</h2>
+                    <h2>Our handcrafted cakes are the perfect addition to any celebration, and we take pride in being the best online cake shop.</h2>
                     <button className="btn">Browse Cakes</button>
                 </div>
                 <figure className="header__img--wrapper">
--- src/Components/Highlights.js
+++ src/Components/Highlights.js
@@ -5,26 +5,30 @@ export default function Highlights (props){
     <div className="big-row">
         <h2 className="section__title">Why Choose <span className="neon-blue">Cake Delights</span> </h2>
         <div className="highlight__wrapper">
-            <div className="highlight">
+        <div className="highlight">
                 <div className="highlight__img">
-                <i class="fa-solid fa-cart-shopping"></i>
-                <h3 className="highlight__subtitle">Place with easily</h3>
-                <p className="highlight__para">Our user-friendly website allows you to effortlessly browse through a wide range of cake options, each designed with meticulous attention to detail by our talented pastry artisans.</p>
+                <i class="fa-solid fa-cake-candles"></i>
                 </div>
+                <h3 className="highlight__subtitle">Quality </h3>
+                <p className="highlight__para">Enjoy a variety of flavors, from classic chocolate and vanilla to bold combinations. We make every cake with the freshest, highest-quality ingredients, ensuring that each slice is a moment of pure bliss.</p>
+                
             </div>
             <div className="highlight">
                 <div className="highlight__img">
                 <i class="fa-solid fa-cart-shopping"></i>
-                <h3 className="highlight__subtitle">Place with easily</h3>
-                <p className="highlight__para">Our user-friendly website allows you to effortlessly browse through a wide range of cake options, each designed with meticulous attention to detail by our talented pastry artisans.</p>
                 </div>
+                <h3 className="highlight__subtitle">Place Order Easily</h3>
+                <p className="highlight__para">Our website has easy-to-use features that let you explore a variety of cakes made by skilled pastry chefs.</p>
+                
             </div>
+            
             <div className="highlight">
                 <div className="highlight__img">
-                <i class="fa-solid fa-cart-shopping"></i>
-                <h3 className="highlight__subtitle">Place with easily</h3>
-                <p className="highlight__para">Our user-friendly website allows you to effortlessly browse through a wide range of cake options, each designed with meticulous attention to detail by our talented pastry artisans.</p>
+                <i class="fa-solid fa-truck"></i>
                 </div>
+                <h3 className="highlight__subtitle">Delivery </h3>
+                <p className="highlight__para">Cake Delights delivers nationwide, ensuring your cake arrives on time and in perfect condition. We dedicate our team to providing excellent customer service, and we're always here to assist you with questions or custom requests you may have.</p>
+                
             </div>
         </div>
     </div>
