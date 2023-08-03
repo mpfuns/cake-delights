f4281235886b142526dbd7ac964ca911f11a5587
Monet <monetpaul23@gmail.com>
2023-08-03 10:41:47 -0400

    working on waves

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Highlights from './Components/Highlights';
 import Features from './Components/Features';
 import logo from './Components/img/Cake Delights (1).png'
 import headerPic from './Components/img/undraw_birthday_cake_re_bsw5.svg'
+import topWave  from "./Components/img/topwave.svg"
+import bottomWave from "./Components/img/bottomwave.svg"
 function App() {
   return (
     <div className="App">
@@ -13,7 +15,17 @@ function App() {
     <Header headerPic={headerPic}/>
     </section>
     <main>
+    <div id="main_wave">
+    <div id='top_wave'>
+    <img  className='wave-img' src={topWave}/>
+    </div>
     <Highlights />
+    <div id='bottom_wave'>
+    <img  src={bottomWave}/>
+    </div>
+    
+    </div>
+    
     <Features />
     </main>
     </div>
